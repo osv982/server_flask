dedd7bad5bc859240d870612139afebcb6726914
osv982 <78878728+osv982@users.noreply.github.com>
2022-07-29 20:38:01 +0300

    Update Models.py

--- Models.py
+++ Models.py
@@ -1,12 +1,17 @@
 from datetime import datetime
 from app import db
+from sqlalchemy import Index
 
 
-class history_users(db.Model):
-    __tablename__ = 'query_history'
+class users_history(db.Model):
+    __tablename__ = 'users_history'
+    history_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     history_user = db.Column(db.Integer, unique=False, nullable=False)
     history_query = db.Column(db.String(max), unique=False, nullable=False)
-    history_date = db.Column(db.DateTime, primary_key=True, nullable=False, default=datetime.utcnow)
+    history_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    Index('idx_history_user_date', 'history_user', 'history_date')
+    # db.Index('idx_history_user_date', 'history_user', 'history_date')
+
 
     def __init__(self, history_user, history_query, history_date=datetime.utcnow):
         self.history_user = history_user
@@ -19,6 +24,8 @@ class history_users(db.Model):
         history_date = self.history_date
         return {"query": history_query, "date": history_date}
 
-    def represent(self):
-        history_user = self.history_user
-        return history_user
+    # def represent(self):
+    #     history_user = self.history_user
+    #     return history_user
+
+db.create_all()
